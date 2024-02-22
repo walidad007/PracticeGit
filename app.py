b3019215cff98d86c1a5fb46f95ec4df5cc7e7dd
walidad007 <walidadmahtam62@gmail.com>
2024-02-22 20:53:59 +0500

    final changes

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def addition(a+b+c):
     return a+b+c 
 
-<<<<<<< HEAD
 def subtraction(a,b,d):
     return a-b-d
-=======
-def subtraction(a,b,e):
-    return a-b-e
->>>>>>> 3d6ce873ffb94716a8691aa20afb0d3e7385649e
 
 def division(a,b):
     return a/b
